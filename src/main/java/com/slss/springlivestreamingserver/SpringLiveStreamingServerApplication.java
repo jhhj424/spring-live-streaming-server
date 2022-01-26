d078741fd0af8c5fc36f3be0ac9e17a8f4d9a5b1
zito-s <jhhj424@naver.com>
2022-01-27 00:32:31 +0900

    Initial commit

---
+++ src/main/java/com/slss/springlivestreamingserver/SpringLiveStreamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.slss.springlivestreamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLiveStreamingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLiveStreamingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/slss/springlivestreamingserver/SpringLiveStreamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slss.springlivestreamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLiveStreamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
